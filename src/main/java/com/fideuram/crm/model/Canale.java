5c2d6a85875d51e4657b85772def84743369e7fe
bolognesimarco <bolognesi.marco@gmail.com>
2014-06-20 14:02:10 +0200

    d

--- src/main/java/com/fideuram/crm/model/Canale.java
+++ src/main/java/com/fideuram/crm/model/Canale.java
@@ -1,5 +1,6 @@
 package com.fideuram.crm.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -8,6 +9,12 @@ public class Canale {
 
 	@Id
 	private int id;
+	
+	@Column
+	private String codice;
+	
+	@Column
+	private String descrizione;
 
 
 	public int getId() {
@@ -16,6 +23,22 @@ public class Canale {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
 	}
 
 	
